43bb1890d50e148c7f7d0874768ed29e32111514
toanleviet95 <toanleviet95@gmail.com>
2016-10-03 22:22:36 +0700

    make it better

--- db/migrate/20161002103704_create_tai_khoans.rb
+++
@@ -1,8 +0,0 @@
-class CreateTaiKhoans < ActiveRecord::Migration[5.0]
-  def change
-    create_table :tai_khoans do |t|
-      t.string :username
-      t.string :password
-    end
-  end
-end
--- db/migrate/20161002114549_create_the_loais.rb
+++
@@ -1,7 +0,0 @@
-class CreateTheLoais < ActiveRecord::Migration[5.0]
-  def change
-    create_table :the_loais do |t|
-      t.string :ten_the_loai
-    end
-  end
-end
