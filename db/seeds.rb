9f13f09576fde0dd906c9b723d54580c3360330c
toanleviet95 <toanleviet95@gmail.com>
2016-10-03 23:05:48 +0700

    update database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+TheLoai.create(ten_the_loai: "Cuộc sống")
+TheLoai.create(ten_the_loai: "Tình yêu")
+TheLoai.create(ten_the_loai: "Hôn nhân & Gia đình")
+TaiKhoan.create(username: "toanleviet", password: "123456")
+TaiKhoan.create(username: "tuongvi", password: "123456")
